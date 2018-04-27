812a79a816fa8980e93f689a5caa496cbf102e44
Christopher Still <chris.still@oregonstate.edu>
2018-04-27 11:50:31 -0700

    Create publications.m

---
+++ publications.m
@@ -0,0 +1,7 @@
+---
+layout: page 
+title: Publications 
+---
+Greer, B. T., *Still, C.*, Cullinan, G. L., Brooks, J. R., & Meinzer, F. C. 2017. Polyploidy influences plant–environment interactions in quaking aspen (Populus tremuloides Michx.). Tree Physiology, 1-11.
+
+Baguskas, S.A., *Still, C.J.*, Fischer, D.T., D’Antonio, C.M., and J.Y. King. 2016. Coastal fog during summer drought improves the water status of sapling trees more than adult trees in a California pine forest. Oecologia, DOI: 10.1007/s00442-016- 3556-y.
